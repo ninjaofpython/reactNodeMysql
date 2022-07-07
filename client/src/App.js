f40aed5e552cb3394d2ee02a642e64ac6f195cec
ninjaofpython <subxim@gmail.com>
2022-07-07 05:19:30 +0000

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import { useState } from "react";
+import Axios from 'axios';
+
+function App() {
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState(0);
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage
+    }).then(() => {
+      setEmployeeList([...employeeList, {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage,
+      },
+      ])
+    })
+  }
+
+  const getEmployees = () => {
+    Axios.get('http://localhost:3001/employees').then((response) => {
+      setEmployeeList(response.data)
+    })
+
+  }
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Job Title:</label>
+        <input type="text" onChange={(event) => {
+          setJobTitle(event.target.value);
+        }} />
+        <label>Email:</label>
+        <input type="text" onChange={(event) => {
+          setEmail(event.target.value);
+        }} />
+        <label>Job URL:</label>
+        <input type="text" onChange={(event) => {
+          setJobUrl(event.target.value);
+        }} />
+        <label>Postion:</label>
+        <input type="text" onChange={(event) => {
+          setPosition(event.target.value);
+        }} />
+        <label>Wage (year):</label>
+        <input type="number" onChange={(event) => {
+          setWage(event.target.value)
+        }} />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
+        {employeeList.map((val, key) => {
+          return <div className="employee">
+            <h3>Name: {val.name}</h3>
+            <h3>Age: {val.age}</h3>
+            <h3>Country: {val.country}</h3>
+            <h3>Position: {val.position}</h3>
+            <h3>Wage: {val.wage}</h3>
+          </div>
+        })}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
